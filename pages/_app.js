e67d15bcb33b0e4370c44e05a13be4c485f3b0b4
mrrobot <federicomigliore87@gmail.comm>
2023-02-06 10:54:36 +0100

    resize

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
 import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
   const [closeSidebar, setCloseSidebar] = useState(false);
   const handleSidebar = () => {
     setCloseSidebar(prev => !prev);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 540) {
+        setCloseSidebar(true);
+      } else {
+        setCloseSidebar(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
       <Sidebar closeSidebar={closeSidebar}>
